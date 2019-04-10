58990cc95f4bff0b6c8cb67f0f288a5ca8eefa76
Ross Chadwick <ross@rchadwick.co.uk>
2019-04-10 14:07:41 +0200

    Increase moisture values to something that wont murder most plants. Pump activates longer too.

--- src/main.h
+++ src/main.h
@@ -15,10 +15,10 @@ const int PINS_MOISTURE[NUM_MOISTURE_SENSORS]= {34, 35};
 
 #define MAX_MOISTURE_VALUE 4095 // Change this to values your sensor reads
 #define MIN_MOISTURE_VALUE 0 // Change this to values your sensor reads
-#define LOW_MOISTURE_TRIGGER 20 // Low moisture level in percentage, which will trigger the pumping process
-#define TARGET_MOISTURE 50 // Target moisture level in percentage
-#define PUMP_SECONDS 15 // Number of seconds to pump for before waiting for another reading
-#define WAIT_SECONDS 15 // Seconds to wait inbetween pumps, before checking the moisture level again
+#define LOW_MOISTURE_TRIGGER 50 // Low moisture level in percentage, which will trigger the pumping process
+#define TARGET_MOISTURE 70 // Target moisture level in percentage
+#define PUMP_SECONDS 20 // Number of seconds to pump for before waiting for another reading
+#define WAIT_SECONDS 20 // Seconds to wait inbetween pumps, before checking the moisture level again
 
 #define OLED_RESET 16
 #define OLED_WIDTH 128
